2f3020df21041aab2a4d4465fbb89cad0477f73f
czen0002 <czen0002@student.monash.edu>
2020-03-15 00:59:14 +1100

    create basic front-end framwork

--- MotionSurveillanceSystem/src/app/app-routing.module.ts
+++ MotionSurveillanceSystem/src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
+import { AppComponent } from './app.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { RecordingComponent } from './recording/recording.component';
+import { ManagementComponent } from './management/management.component';
+import { LiveComponent } from './live/live.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LiveComponent
+  },
+  {
+    path: 'recording',
+    component: RecordingComponent
+  },
+  {
+    path: 'management',
+    component: ManagementComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**', redirectTo: ''
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MotionSurveillanceSystem/src/app/app.module.ts
+++ MotionSurveillanceSystem/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LiveComponent } from './live/live.component';
+import { RecordingComponent } from './recording/recording.component';
+import { ManagementComponent } from './management/management.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LiveComponent,
+    RecordingComponent,
+    ManagementComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
