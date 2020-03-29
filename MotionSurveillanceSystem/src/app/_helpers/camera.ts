816133a841837831a8381185f0ad95056b3702d4
czen0002 <czen0002@student.monash.edu>
2020-03-29 11:51:31 +1100

    add start and end time property in Camera; update dummy data

--- MotionSurveillanceSystem/src/app/_helpers/camera.ts
+++ MotionSurveillanceSystem/src/app/_helpers/camera.ts
@@ -2,4 +2,6 @@ export interface Camera {
     cameraID: number;
     cameraLocation: string;
     cameraClient: string;
+    startTime: Object;
+    endTime: Object;
 }
--- MotionSurveillanceSystem/src/app/_helpers/cameras.ts
+++ MotionSurveillanceSystem/src/app/_helpers/cameras.ts
@@ -5,81 +5,113 @@ export const Cameras: Camera[] = [
   {
     cameraID: 1,
     cameraLocation: "Melbourne, VIC",
-    cameraClient: "Monash University"
+    cameraClient: "Monash University",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 2,
     cameraLocation: "Melbourne, VIC",
-    cameraClient: "Monash University"
+    cameraClient: "Monash University",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 3,
     cameraLocation: "Melbourne, VIC",
-    cameraClient: "The University of Melbourne"
+    cameraClient: "The University of Melbourne",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 4,
     cameraLocation: "Melbourne, VIC",
-    cameraClient: "The University of Melbourne"
+    cameraClient: "The University of Melbourne",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 5,
     cameraLocation: "Sydney, NSW",
-    cameraClient: "The University of Sydney"
+    cameraClient: "The University of Sydney",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 6,
     cameraLocation: "Sydney, NSW",
-    cameraClient: "The University of Sydney"
+    cameraClient: "The University of Sydney",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 7,
     cameraLocation: "Sydney, NSW",
-    cameraClient: "The University of New South Wales"
+    cameraClient: "The University of New South Wales",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 8,
     cameraLocation: "Sydney, NSW",
-    cameraClient: "The University of New South Wales"
+    cameraClient: "The University of New South Wales",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 9,
     cameraLocation: "Hobart, TAS",
-    cameraClient: "University of Tasmania"
+    cameraClient: "University of Tasmania",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 10,
     cameraLocation: "Hobart, TAS",
-    cameraClient: "University of Tasmania"
+    cameraClient: "University of Tasmania",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 11,
     cameraLocation: "Canberra, ACT",
-    cameraClient: "The Australian National University"
+    cameraClient: "The Australian National University",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 12,
     cameraLocation: "Canberra, ACT",
-    cameraClient: "The Australian National University"
+    cameraClient: "The Australian National University",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 13,
     cameraLocation: "Brisbane, QLD",
-    cameraClient: "The University of Queensland"
+    cameraClient: "The University of Queensland",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 14,
     cameraLocation: "Brisbane, QLD",
-    cameraClient: "The University of Queensland"
+    cameraClient: "The University of Queensland",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 15,
     cameraLocation: "Adelaide, SA",
-    cameraClient: "The University of Adelaide"
+    cameraClient: "The University of Adelaide",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
   {
     cameraID: 16,
     cameraLocation: "Adelaide, SA",
-    cameraClient: "The University of Adelaide"
+    cameraClient: "The University of Adelaide",
+    startTime: {hour: 23, minute: 0},
+    endTime: {hour: 7, minute: 0}
   },
 ];
