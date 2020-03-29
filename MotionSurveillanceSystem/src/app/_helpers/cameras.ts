a2b388be2b317d9bc255ec435b4a872185d45d4b
czen0002 <czen0002@student.monash.edu>
2020-03-29 11:15:59 +1100

    create dummp camera data in cameras.ts

---
+++ MotionSurveillanceSystem/src/app/_helpers/cameras.ts
@@ -0,0 +1,85 @@
+import { Camera } from './camera';
+
+
+export const Cameras: Camera[] = [
+  {
+    cameraID: 1,
+    cameraLocation: "Melbourne, VIC",
+    cameraClient: "Monash University"
+  },
+  {
+    cameraID: 2,
+    cameraLocation: "Melbourne, VIC",
+    cameraClient: "Monash University"
+  },
+  {
+    cameraID: 3,
+    cameraLocation: "Melbourne, VIC",
+    cameraClient: "The University of Melbourne"
+  },
+  {
+    cameraID: 4,
+    cameraLocation: "Melbourne, VIC",
+    cameraClient: "The University of Melbourne"
+  },
+  {
+    cameraID: 5,
+    cameraLocation: "Sydney, NSW",
+    cameraClient: "The University of Sydney"
+  },
+  {
+    cameraID: 6,
+    cameraLocation: "Sydney, NSW",
+    cameraClient: "The University of Sydney"
+  },
+  {
+    cameraID: 7,
+    cameraLocation: "Sydney, NSW",
+    cameraClient: "The University of New South Wales"
+  },
+  {
+    cameraID: 8,
+    cameraLocation: "Sydney, NSW",
+    cameraClient: "The University of New South Wales"
+  },
+  {
+    cameraID: 9,
+    cameraLocation: "Hobart, TAS",
+    cameraClient: "University of Tasmania"
+  },
+  {
+    cameraID: 10,
+    cameraLocation: "Hobart, TAS",
+    cameraClient: "University of Tasmania"
+  },
+  {
+    cameraID: 11,
+    cameraLocation: "Canberra, ACT",
+    cameraClient: "The Australian National University"
+  },
+  {
+    cameraID: 12,
+    cameraLocation: "Canberra, ACT",
+    cameraClient: "The Australian National University"
+  },
+  {
+    cameraID: 13,
+    cameraLocation: "Brisbane, QLD",
+    cameraClient: "The University of Queensland"
+  },
+  {
+    cameraID: 14,
+    cameraLocation: "Brisbane, QLD",
+    cameraClient: "The University of Queensland"
+  },
+  {
+    cameraID: 15,
+    cameraLocation: "Adelaide, SA",
+    cameraClient: "The University of Adelaide"
+  },
+  {
+    cameraID: 16,
+    cameraLocation: "Adelaide, SA",
+    cameraClient: "The University of Adelaide"
+  },
+];
