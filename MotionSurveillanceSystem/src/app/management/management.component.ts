09373f80604a7aafd78007997213ed45ad12dff0
czen0002 <czen0002@student.monash.edu>
2020-03-29 12:45:54 +1100

    camera details shows below table after clicking edit button

--- MotionSurveillanceSystem/src/app/management/management.component.ts
+++ MotionSurveillanceSystem/src/app/management/management.component.ts
@@ -13,6 +13,10 @@ import {NgbdSortableHeader, SortEvent} from '../_helpers/sortable.directive';
 })
 export class ManagementComponent implements OnInit {
 
+  cameraID: number;
+  cameraClient: string;
+  cameraLocation: string;
+  
   ngOnInit() {
   }
 
@@ -33,9 +37,19 @@ export class ManagementComponent implements OnInit {
         header.direction = '';
       }
     });
-
     this.service.sortColumn = column;
     this.service.sortDirection = direction;
   }
 
+  onSelectUpdate(camera){
+    this.cameraID = camera.cameraID;
+    this.cameraClient = camera.cameraClient;
+    this.cameraLocation = camera.cameraLocation;
+    console.log(this.cameraID);
+    console.log(this.cameraClient);
+    console.log(this.cameraLocation);
+    
+    
+    
+  }
 }
