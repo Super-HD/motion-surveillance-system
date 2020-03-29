c19ad414dfd2e5344024f6e01d2575cb24762e5b
czen0002 <czen0002@student.monash.edu>
2020-03-29 15:33:12 +1100

    create NgbTimeStruct, the default time struct whoes values are number

---
+++ MotionSurveillanceSystem/src/app/_helpers/ngb-time-struct.ts
@@ -0,0 +1,14 @@
+/**
+ * An interface for the time model used by the timepicker.
+ */
+export interface NgbTimeStruct {
+    /**
+     * The hour in the `[0, 23]` range.
+     */
+    hour: number;
+  
+    /**
+     * The minute in the `[0, 59]` range.
+     */
+    minute: number;
+  }
