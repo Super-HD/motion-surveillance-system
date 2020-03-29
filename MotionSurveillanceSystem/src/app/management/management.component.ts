e8ee4341b6c328ddc7ed7a8ed807d0d5289d5deb
czen0002 <czen0002@student.monash.edu>
2020-03-29 11:38:57 +1100

    update management component

--- MotionSurveillanceSystem/src/app/management/management.component.ts
+++ MotionSurveillanceSystem/src/app/management/management.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import {Observable} from 'rxjs';
+
+import { Camera } from './../_helpers/camera';
+import { CameraService } from './../_services/camera.service';
+import {NgbdSortableHeader, SortEvent} from '../_helpers/sortable.directive';
 
 @Component({
   selector: 'app-management',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagementComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
+  }
+
+  cameras$: Observable<Camera[]>;
+  total$: Observable<number>;
+
+  @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
+
+  constructor(public service: CameraService) {
+    this.cameras$ = service.cameras$;
+    this.total$ = service.total$;
+  }
+
+  onSort({column, direction}: SortEvent) {
+    // resetting other headers
+    this.headers.forEach(header => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    this.service.sortColumn = column;
+    this.service.sortDirection = direction;
   }
 
 }
