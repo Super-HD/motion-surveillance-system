d27609bbd5bc1e8993abf1ad1ac1d581cbe2d7fd
czen0002 <czen0002@student.monash.edu>
2020-04-05 16:05:28 +1000

    camera schema is created in camera.js

--- MotionSurveillanceSystem/models/camera.js
+++ MotionSurveillanceSystem/models/camera.js
@@ -1,1 +1,28 @@
+import { truncate } from 'fs';
+import { stringify } from 'querystring';
+
 const mongoose = require('mongoose');
+
+const cameraSchema = new mongoose.Schema({
+    _id: mongoose.model.Schema.Types.ObjectID,
+    cameraID: {
+        type: Number,
+        required: true
+    },
+    cameraLocation: {
+        type: String,
+        required: true
+    },
+    cameraClient: {
+        type: String,
+        required: true
+    },
+    startTime: {
+        type: Object
+    },
+    endTime: {
+        type: Object
+    }
+});
+
+module.exports = mongoose.model('Camera', cameraSchema);
