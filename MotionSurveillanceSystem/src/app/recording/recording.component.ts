062618ad14b87df91a62545afe4541343f4dec09
czen0002 <czen0002@student.monash.edu>
2020-04-08 00:50:38 +1000

    modify recording section; recording data from database

--- MotionSurveillanceSystem/src/app/recording/recording.component.ts
+++ MotionSurveillanceSystem/src/app/recording/recording.component.ts
@@ -1,12 +1,12 @@
 
 import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
-import {DecimalPipe} from '@angular/common';
-import {Observable} from 'rxjs';
+// import {Observable} from 'rxjs';
 import { NgxSmartModalService } from 'ngx-smart-modal';
+import { ClipsService} from '../clipsservice/clips.service';
 
-import {Clip} from '../_helpers/clip';
-import {ClipService} from '../_services/clip.service';
-import {NgbdSortableHeader, SortEvent} from '../_helpers/sortable.directive';
+// import {Clip} from '../_helpers/clip';
+// import {ClipService} from '../_services/clip.service';
+// import {NgbdSortableHeader, SortEvent} from '../_helpers/sortable.directive';
 
 @Component({
   selector: 'app-recording',
@@ -15,30 +15,42 @@ import {NgbdSortableHeader, SortEvent} from '../_helpers/sortable.directive';
 })
 export class RecordingComponent implements OnInit {
 
+  clipsDB: Array<any> = [];
+
+  constructor(private clipService: ClipsService, public ngxSmartModalService: NgxSmartModalService) {}
+
   ngOnInit() {
+    this.onGetClips();
   }
 
-  clips$: Observable<Clip[]>;
-  total$: Observable<number>;
-
-  @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
-
-  constructor(public service: ClipService,
-    public ngxSmartModalService: NgxSmartModalService) {
-    this.clips$ = service.clips$;
-    this.total$ = service.total$;
-  }
-
-  onSort({column, direction}: SortEvent) {
-    // resetting other headers
-    this.headers.forEach(header => {
-      if (header.sortable !== column) {
-        header.direction = '';
-      }
+  onGetClips() {
+    this.clipService.getClips().subscribe((data:any[])=>{
+      this.clipsDB = data;
+      console.log(this.clipsDB);
     });
-
-    this.service.sortColumn = column;
-    this.service.sortDirection = direction;
   }
+
+  // clips$: Observable<Clip[]>;
+  // total$: Observable<number>;
+
+  // @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
+
+  // constructor(public service: ClipService,
+  //   public ngxSmartModalService: NgxSmartModalService) {
+  //   this.clips$ = service.clips$;
+  //   this.total$ = service.total$;
+  // }
+
+  // onSort({column, direction}: SortEvent) {
+  //   // resetting other headers
+  //   this.headers.forEach(header => {
+  //     if (header.sortable !== column) {
+  //       header.direction = '';
+  //     }
+  //   });
+
+  //   this.service.sortColumn = column;
+  //   this.service.sortDirection = direction;
+  // }
 
 }
