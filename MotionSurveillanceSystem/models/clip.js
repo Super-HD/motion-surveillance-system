053402231343e5b40b656ed690d0375cfd37a744
czen0002 <czen0002@student.monash.edu>
2020-04-08 00:49:19 +1000

    change clip schema; recordingDate type

--- MotionSurveillanceSystem/models/clip.js
+++ MotionSurveillanceSystem/models/clip.js
@@ -11,7 +11,7 @@ const clipSchema = new mongoose.Schema({
         required: true
     },
     recordingDate: {
-        type: Date,
+        type: String,
         required: true
     },
     clipLink: {
