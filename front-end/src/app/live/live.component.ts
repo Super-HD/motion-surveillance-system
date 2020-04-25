8d64c3ba440f2118a5c9e6dcdb57aea4ba345008
czen0002 <czen0002@student.monash.edu>
2020-04-26 03:48:53 +1000

    initial streaming on live section

--- front-end/src/app/live/live.component.ts
+++ front-end/src/app/live/live.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { StreamingService } from './../streamingservice/streaming.service';
 
 @Component({
   selector: 'app-live',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./live.component.css']
 })
 export class LiveComponent implements OnInit {
+  // To-do: use camerasservice to get all cameras' port number
+  // Each port number has its stream source
+  streamSource: string = "http://localhost:5100";
+  img: any;
 
-  constructor() { }
+  constructor(private streamingService: StreamingService) { }
 
   ngOnInit() {
+    this.streamingService.getStream(this.streamSource).subscribe((img) => {
+      this.img = "data:image/jpeg;base64,"+ img;
+    })
+
   }
 
 }
