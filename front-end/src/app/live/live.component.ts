212664b4052d13beefb81134d8f78dd345f3ee9b
czen0002 <czen0002@student.monash.edu>
2020-04-27 03:08:35 +1000

    get streaming sources from database

--- front-end/src/app/live/live.component.ts
+++ front-end/src/app/live/live.component.ts
@@ -1,6 +1,9 @@
 
 import { Component, OnInit } from '@angular/core';
 import { StreamingService } from './../streamingservice/streaming.service';
+import { CamerasService } from '../camerasservice/cameras.service';
+import { Camera } from "../../../../back-end/models/Camera";
+import { CompileMetadataResolver } from '@angular/compiler';
 
 @Component({
   selector: 'app-live',
@@ -8,18 +11,30 @@ import { StreamingService } from './../streamingservice/streaming.service';
   styleUrls: ['./live.component.css']
 })
 export class LiveComponent implements OnInit {
-  // To-do: use camerasservice to get all cameras' port number
-  // Each port number has its stream source
-  streamSource: string = "http://localhost:5100";
-  img: any;
+  camerasDB: Camera[] = [];
+  camerasURL = [];
 
-  constructor(private streamingService: StreamingService) { }
+  constructor(private streamingService: StreamingService, private cameraService: CamerasService) { }
 
   ngOnInit() {
-    this.streamingService.getStream(this.streamSource).subscribe((img) => {
-      this.img = "data:image/jpeg;base64,"+ img;
-    })
-
+    this.onGetStreams();
   }
-
+  // To-do: show camera info 
+  onGetStreams() {
+    this.cameraService.getCameras().subscribe((data:any[])=>{
+      this.camerasDB = data;
+      console.log(this.camerasDB);
+      for (let i = 0; i <= this.camerasDB.length/3; i++){
+        this.camerasURL.push([])
+      }
+      // var temp = [];
+      for (let i = 0; i < this.camerasDB.length; i++) {
+        console.log(i/3);
+        this.streamingService.getStream(this.camerasDB[i].cameraURL).subscribe(img => {
+          this.camerasURL[Math.floor(i/3)][i%3] = "data:image/jpeg;base64,"+ img;
+        })
+      }
+    });
+    console.log(this.camerasURL);
+  }
 }
