63abd03aac1193ddb848764f6f3bd57f9b8306b6
chengzeng <czen0002@student.monash.edu>
2020-05-29 15:11:44 +1000

    add comments in live.components.ts

--- front-end/src/app/live/live.component.ts
+++ front-end/src/app/live/live.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { StreamingService } from './../streamingservice/streaming.service';
 import { CamerasService } from '../camerasservice/cameras.service';
 import { Camera } from "../../../../back-end/models/Camera";
-import { CompileMetadataResolver } from '@angular/compiler';
 
 @Component({
   selector: 'app-live',
@@ -27,10 +26,14 @@ export class LiveComponent implements OnInit {
   // To-do: hide cameras not running
   onGetStreams() {
     this.cameraService.getCameras().subscribe((data: any[]) => {
+      // Get all cameras data form mongodb
       this.camerasDB = data;
+      // Create a new empty array of length 3
+      // Append this new empty array to camerasObj
       for (let i = 0; i <= this.camerasDB.length / 3; i++) {
         this.camerasObj.push([]);
       }
+      // Create cameraOBj using camera's info, put it in the nested array in camerasObj
       for (let i = 0; i < this.camerasDB.length; i++) {
         this.camerasObj[Math.floor(i / 3)][i % 3] = {
           URL: "",
@@ -39,10 +42,8 @@ export class LiveComponent implements OnInit {
         };
       }
       for (let i = 0; i < this.camerasDB.length; i++) {
-
-        // this.camerasDB[i].cameraURL
+        // Get camera streaming using cameraURL from each camera object created below
         this.streamingService.getStream(this.camerasDB[i].cameraURL).subscribe(img => {
-          // this.cameraObj.URL = "data:image/jpeg;base64,"+ img;
           this.camerasObj[Math.floor(i / 3)][i % 3].URL = "data:image/jpeg;base64," + img;
         })
       }
