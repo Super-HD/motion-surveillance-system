bddbaf173944fec3111a10e3176171132d17d03d
Terence Ng Han How <ngtere@live.com.sg>
2020-05-29 17:47:50 +1000

    Big Changes

---
+++ clients/s3-file-upload.js
@@ -0,0 +1,42 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
+const BUCKET_NAME = 'terencenghan-bucket';
+const IAM_USER_KEY = 'AKIAJ4MHIRQ3R3NJPN6Q';
+const IAM_USER_SECRET = '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';
+
+let s3bucket = new AWS.S3({
+  accessKeyId: IAM_USER_KEY,
+  secretAccessKey: IAM_USER_SECRET,
+  Bucket: BUCKET_NAME
+});
+
+function uploadToS3(videoFile, axios, cameraId) {
+
+  const fileContent = fs.readFileSync(videoFile)
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: 'testvideo.mp4',
+    Body: fileContent
+  }
+  s3bucket.upload(params, (err, data) => {
+    if (err) {
+      console.log('error in callback');
+      console.log(err);
+    }
+    console.log('success')
+    console.log(data.Location)
+
+    const newClip = {
+      camera: cameraId,
+      recordingDate: "testing",
+      clipLink: data.Key
+    }
+    // POST Request to DigitalOcean Server to store in MongoDB
+    axios.post('http://161.35.110.201:4200/clip', { ...newClip })
+  })
+}
+
+module.exports = {
+  uploadToS3
+}
