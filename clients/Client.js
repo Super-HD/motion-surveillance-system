1e224089fcd1ea20dd45e7ad23188ed288bfa9dc
ShawnYPS <pyan0005@student.monash.edu>
2020-06-03 20:33:11 +1000

    time setting function

--- clients/Client.js
+++ clients/Client.js
@@ -34,7 +34,7 @@ function writeVedio(time,count){
   var end_time;
   var stop = false;
   var frame,gray;
-  var writer =new cv.VideoWriter(video_name, cv.VideoWriter.fourcc('MJPG'),24.0,new cv.Size(vCap.get(cv.CAP_PROP_FRAME_WIDTH),vCap.get(cv.CAP_PROP_FRAME_HEIGHT)));
+  var writer =new cv.VideoWriter(video_name, cv.VideoWriter.fourcc('MJPG'),10.0,new cv.Size(vCap.get(cv.CAP_PROP_FRAME_WIDTH),vCap.get(cv.CAP_PROP_FRAME_HEIGHT)));
    {
      while(stop == false){
       frame = vCap.read()
@@ -53,8 +53,50 @@ function writeVedio(time,count){
 }
 
 function algorithm(){
+
+  var start_time,end_time,start_time_hr,start_time_min,end_time_hr,end_time_min;
+  var today = new Date();
+  var current_time = Number(today.getHours().toString() + today.getMinutes().toString());
   var firstFrame, frameDelta, gray, thresh;
 
+  start_time_hr = 20
+  start_time_min = 29
+  end_time_hr = 12
+  end_time_min = 0
+  if(start_time_min == 0){
+    start_time = Number(start_time_hr.toString() + start_time_min.toString() + "0");  
+  }
+  else if(start_time_min.toString().length == 1){
+    start_time = Number(start_time_hr.toString() + "0" +start_time_min.toString());  
+  }
+  else{
+    start_time = Number(start_time_hr.toString() + start_time_min.toString());  
+  }
+
+  if(end_time_min == 0){
+    end_time = Number(end_time_hr.toString() + end_time_min.toString() + "0");  
+  }
+  else if(end_time_min.toString().length == 1){
+    end_time = Number(end_time_hr.toString() + "0" +end_time_min.toString());  
+  }
+  else{
+    end_time = Number(end_time_hr.toString() + end_time_min.toString());  
+  }
+
+  if(today.getMinutes() == 0){
+    current_time = Number(today.getHours().toString() + today.getMinutes().toString() + "0");
+  }
+  else if(today.getMinutes().toString().length == 1){
+    end_time = Number(today.getHours().toString() +"0" +today.getMinutes().toString());
+  }
+  else{
+    current_time = Number(today.getHours().toString() + today.getMinutes().toString());
+  }
+  
+
+  console.log(start_time)
+  console.log(end_time)
+  console.log(current_time)
   var write = false;
   var video_count = 0;
   frame = vCap.read();
@@ -63,24 +105,29 @@ function algorithm(){
   firstFrame = frame.cvtColor(cv.COLOR_BGR2GRAY);
   firstFrame = firstFrame.gaussianBlur(new cv.Size(21, 21),0);
 
-
   interval = setInterval(function() {
-    if(write == false){
+
+    if(today.getMinutes() == 0){
+      current_time = Number(today.getHours().toString() + today.getMinutes().toString() + "0");
+    }
+    else if(today.getMinutes().toString().length == 1){
+      end_time = Number(today.getHours().toString() +"0" +today.getMinutes().toString());
+    }
+    else{
+      current_time = Number(today.getHours().toString() + today.getMinutes().toString());
+    }
+      if(start_time == end_time){
+        if(write == false){
           frame = vCap.read();
           gray = frame.cvtColor(cv.COLOR_BGR2GRAY);
           gray = gray.gaussianBlur(new cv.Size(21, 21),0);
-
           //compute difference between first frame and current frame
           frameDelta = firstFrame.absdiff(gray);
           thresh = frameDelta.threshold(25,255, cv.THRESH_BINARY);
           thresh = thresh.dilate(new cv.Mat(), new cv.Vec(-1, -1), 2);
-
           var cnts = thresh.findContours(cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
           for(i = 0; i < cnts.length; i++) {
-
-              if(cnts[i].area < 500) {
-                  continue;
-              }
+            if(cnts[i].area < 500) {continue;}
               write = true;
               console.log("motion detected");
           }
@@ -90,25 +137,85 @@ function algorithm(){
           firstFrame = frame.cvtColor(cv.COLOR_BGR2GRAY);
           firstFrame = firstFrame.gaussianBlur(new cv.Size(21, 21),0);
         }
-      else{
-        writeVedio(10,video_count);
-        video_count +=1;
-        write = false;
-
-
-
-        frame = vCap.read();
-        firstFrame = frame;
-        //convert to grayscale
-        firstFrame = frame.cvtColor(cv.COLOR_BGR2GRAY);
-        firstFrame = firstFrame.gaussianBlur(new cv.Size(21, 21),0);
-
+        else{
+          writeVedio(10,video_count);
+          video_count +=1;
+          write = false;
+          frame = vCap.read();
+          firstFrame = frame;
+          //convert to grayscale
+          firstFrame = frame.cvtColor(cv.COLOR_BGR2GRAY);
+          firstFrame = firstFrame.gaussianBlur(new cv.Size(21, 21),0);
+        }
       }
-           //   clearInterval(interval);
-
-
-  }, 20);
-
+      else if(start_time < end_time){
+        if(current_time >start_time && current_time < end_time){
+          if(write == false){
+            frame = vCap.read();
+            gray = frame.cvtColor(cv.COLOR_BGR2GRAY);
+            gray = gray.gaussianBlur(new cv.Size(21, 21),0);
+            //compute difference between first frame and current frame
+            frameDelta = firstFrame.absdiff(gray);
+            thresh = frameDelta.threshold(25,255, cv.THRESH_BINARY);
+            thresh = thresh.dilate(new cv.Mat(), new cv.Vec(-1, -1), 2);
+            var cnts = thresh.findContours(cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+            for(i = 0; i < cnts.length; i++) {
+              if(cnts[i].area < 500) {continue;}
+                write = true;
+                console.log("motion detected");
+            }
+            frame = vCap.read();
+            firstFrame = frame;
+            //convert to grayscale
+            firstFrame = frame.cvtColor(cv.COLOR_BGR2GRAY);
+            firstFrame = firstFrame.gaussianBlur(new cv.Size(21, 21),0);
+          }
+          else{
+            writeVedio(10,video_count);
+            video_count +=1;
+            write = false;
+            frame = vCap.read();
+            firstFrame = frame;
+            //convert to grayscale
+            firstFrame = frame.cvtColor(cv.COLOR_BGR2GRAY);
+            firstFrame = firstFrame.gaussianBlur(new cv.Size(21, 21),0);
+          }
+        }
+      }else if(start_time > end_time){
+        if((current_time > start_time) || (current_time < end_time)){
+          if(write == false){
+            frame = vCap.read();
+            gray = frame.cvtColor(cv.COLOR_BGR2GRAY);
+            gray = gray.gaussianBlur(new cv.Size(21, 21),0);
+            //compute difference between first frame and current frame
+            frameDelta = firstFrame.absdiff(gray);
+            thresh = frameDelta.threshold(25,255, cv.THRESH_BINARY);
+            thresh = thresh.dilate(new cv.Mat(), new cv.Vec(-1, -1), 2);
+            var cnts = thresh.findContours(cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+            for(i = 0; i < cnts.length; i++) {
+              if(cnts[i].area < 500) {continue;}
+                write = true;
+                console.log("motion detected");
+            }
+            frame = vCap.read();
+            firstFrame = frame;
+            //convert to grayscale
+            firstFrame = frame.cvtColor(cv.COLOR_BGR2GRAY);
+            firstFrame = firstFrame.gaussianBlur(new cv.Size(21, 21),0);
+          }
+          else{
+            writeVedio(10,video_count);
+            video_count +=1;
+            write = false;
+            frame = vCap.read();
+            firstFrame = frame;
+            //convert to grayscale
+            firstFrame = frame.cvtColor(cv.COLOR_BGR2GRAY);
+            firstFrame = firstFrame.gaussianBlur(new cv.Size(21, 21),0);
+          }
+        }
+      }
+    }, 20);
 }
 
 async function doSetup() {
