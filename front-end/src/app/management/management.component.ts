cd01172376d9a6df019680eed5006170266ea842
czen0002 <czen0002@student.monash.edu>
2020-06-12 02:05:03 +1000

    refactor management component

--- front-end/src/app/management/management.component.ts
+++ front-end/src/app/management/management.component.ts
@@ -25,7 +25,7 @@ export class ManagementComponent implements OnInit {
 
   public camerasDB: Camera[] = [];  // An array stores obtained cameras from database
 
-  // variables for pagination
+  // Pagination-controls configuration
   public maxSize: number = 7;
   public directionLinks: boolean = true;
   public autoHide: boolean = false;
@@ -44,10 +44,6 @@ export class ManagementComponent implements OnInit {
   };
   public eventLog: string[] = [];
 
-  // variables for filter
-  _listFilter = "";
-  filteredCameras: Camera[] = []
-
   constructor(private cameraService: CamerasService, 
     private http: HttpClient,
     public fb: FormBuilder){
@@ -55,10 +51,12 @@ export class ManagementComponent implements OnInit {
 
   ngOnInit() {
     this.onGetCameras();
-    this.listFilter = '';
+    this.listFilter = '';  // set listFilter to call filter functions
   }
   
-  // Get all cameras' data from mongodb using camera service
+  /**
+   * Get all camera documents
+   */
   onGetCameras() {
     this.cameraService.getCameras().subscribe((data:any[])=>{
       this.camerasDB = data;
@@ -67,7 +65,8 @@ export class ManagementComponent implements OnInit {
     });
   }
 
-  // Pagination functions
+  // ngx-pagination
+  // From http://michaelbromley.github.io/ngx-pagination/#/advanced
   onPageChange(number: number) {
     this.logEvent(`pageChange(${number})`);
     this.config.currentPage = number;
@@ -82,16 +81,31 @@ export class ManagementComponent implements OnInit {
     this.eventLog.unshift(`${new Date().toISOString()}: ${message}`)
   }
 
-  // Filter functions
+  // Filter
+  // variables for filter
+  _listFilter = "";
+  filteredCameras: Camera[] = []
+
+  /**
+   * Getter function, get _listFilter
+   */
   get listFilter(): string {
     return this._listFilter;
   }
 
+  /** 
+   * Setter function, do filter when there is anattempt to set listFilter
+   */
   set listFilter(value: string) {
     this._listFilter = value;
     this.filteredCameras = this.listFilter ? this.doFilter(this.listFilter) : this.camerasDB;
   }
   
+  /**
+   * Filter Cameras using keyword 
+   * @param filterBy Filter keyword
+   * @return The cameras of an array that meet the condition specified in a callback function
+   */
   doFilter(filterBy: string): Camera[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.camerasDB.filter((camera: Camera) =>
@@ -100,16 +114,20 @@ export class ManagementComponent implements OnInit {
         camera.cameraLocation.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
 
-  // Pre-fill text field for updating
+  /**
+   * Assign values to defined values regarding camera
+   * @param camera A camera json object
+   */
   onSelectUpdate(camera){
-    //this.cameraClient = camera.cameraClient.clientName;
     this.cameraLocation = camera.cameraLocation;
     this.startTime = {hour: Number(camera.startTime.hour), minute: Number(camera.startTime.minute)};
     this.endTime = {hour: Number(camera.endTime.hour), minute: Number(camera.endTime.minute)};
     this.id = camera._id;
   }
 
-  // Update camera info in the database
+  /**
+   * Updata a camera document
+   */
   onUpdateCamera(){
     // Prepare the new camera obj which will be sent to database
     let obj = {
@@ -123,7 +141,11 @@ export class ManagementComponent implements OnInit {
     });
   }
 
-  // Converts a NgbTimeStruct value into CustomTimeStruct value in order to store time info
+  /**
+   * Convert a NgbTimeStruct value into CustomTimeStruct value in order to store time info
+   * @param time A time NgbTimeStruct
+   * @return The CustomTimeStruct converted from input param
+   */
   toModel(time: NgbTimeStruct | null): CustomTimeStruct | null {
     return (time && Number.isInteger(time.hour) && Number.isInteger(time.minute)) ?
         {hour: ("0" + time.hour).slice(-2), minute: ("0" + time.minute).slice(-2)} :
