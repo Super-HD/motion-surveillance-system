928a377583e43350e005f2fe122b904b5d1286f1
czen0002 <czen0002@student.monash.edu>
2020-06-12 01:07:30 +1000

    refactor live component

--- front-end/src/app/live/live.component.ts
+++ front-end/src/app/live/live.component.ts
@@ -13,6 +13,7 @@ export class LiveComponent implements OnInit {
 
   camerasDB: Camera[] = [];  // An array stores obtained cameras from database
   camerasObj = []  // An array of length 3, it is used to store three camera object that will be diplayed in a row
+  // A camera object which contains three values
   cameraObj = {
     URL: "",
     Client: "",
@@ -25,17 +26,19 @@ export class LiveComponent implements OnInit {
     this.onGetStreams();
   }
 
-  // To-do: hide cameras not running
+  /**
+   * Start camera streaming 
+   */
   onGetStreams() {
     this.cameraService.getCameras().subscribe((data: any[]) => {
-      // Get all cameras data form mongodb
+      // Get all cameras documents from MongoDB database
       this.camerasDB = data;
       // Create a new empty array of length 3
       // Append this new empty array to camerasObj
       for (let i = 0; i <= this.camerasDB.length / 3; i++) {
         this.camerasObj.push([]);
       }
-      // Create cameraOBj using camera's info, put it in the nested array in camerasObj
+      // Create camera object using camera's info, put it in the nested array in camerasObj
       for (let i = 0; i < this.camerasDB.length; i++) {
         this.camerasObj[Math.floor(i / 3)][i % 3] = {
           URL: "",
@@ -44,7 +47,7 @@ export class LiveComponent implements OnInit {
         };
       }
       for (let i = 0; i < this.camerasDB.length; i++) {
-        // Get camera streaming using cameraURL from each camera object created below
+        // Get camera streaming frame image calling getStream function
         this.streamingService.getStream(this.camerasDB[i].cameraURL).subscribe(img => {
           this.camerasObj[Math.floor(i / 3)][i % 3].URL = "data:image/jpeg;base64," + img;
         })
